73f783465502d48cdd75978f5e557e099f4a5ee8
Wyn Ponder <wponder@captechventures.com>
2021-04-02 14:46:37 -0400

    Changing API ingest from JavaScript to JQuery

--- vaccines.js
+++ vaccines.js
@@ -1,15 +1,8 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
+// Will get JSON from the url that's passed - will do it after page has loaded
+// Whenever the document is ready, here's a function to call - function is called a callback function - function gets called when something completes - in this case when the document is ready is whenever the DOM is ready
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/vaccinations.json', true)
-
-request.onload = function () {
-  // Begin accessing JSON data here
-}
-
-// Send request
-request.send()
-
-// Access data
-var data = JSON.parse(this.response)
+$(document).ready(function () {
+    $.getJSON('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json', function(data) {
+        console.log(data);
+    })
+})
