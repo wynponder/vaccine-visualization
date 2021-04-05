2448cbefa6fba58c2605c8f28ad5b4f5b2c47e37
Wyn Ponder <wponder@captechventures.com>
2021-04-04 22:42:34 -0400

    Changes to API Code

--- vaccines.js
+++ vaccines.js
@@ -1,5 +1,5 @@
 // Will get JSON from the url that's passed - will do it after page has loaded
-// Whenever the document is ready, here's a function to call - function is called a callback function - function gets called when something completes - in this case when the document is ready is whenever the DOM is ready
+// Whenever the document is ready, we use a callback function to call the data - callback function gets called when something completes - in this case when the document is ready is whenever the DOM is ready
 
 $(document).ready(function () {
     $.getJSON('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json', function(data) {
