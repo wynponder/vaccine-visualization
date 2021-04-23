825060f4a3e633221d8f38334f920cf242503e66
Wyn Ponder <wponder@captechventures.com>
2021-04-22 22:11:05 -0400

    API Connection changes from Stack Overflow

--- vaccines.js
+++ vaccines.js
@@ -1,8 +1,27 @@
-// Will get JSON from the url that's passed - will do it after page has loaded
-// Whenever the document is ready, we use a callback function to call the data - callback function gets called when something completes - in this case when the document is ready is whenever the DOM is ready
+/*var url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json';
 
-$(document).ready(function () {
-    $.getJSON('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json', function(data) {
-        console.log(data);
-    })
-})
+function setup(){
+    noCanvas();
+    loadJSON(url,gotData);
+}
+
+//needs an argument to fill the variable with the data
+function gotData(data){
+    var vaccineData = data.country[164];
+    for (var i = 0; i<vaccineData.length; i++) {
+        createElement('h1',vaccineData.total_vaccinations);
+    }
+}
+println(data.country.total_vaccinations)*/
+
+var settings = {
+    "url": "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json",
+    "method": "GET",
+    "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+    const USA = JSON.parse(response).find((data) => data.country === 'United States');
+    const peopleVaccinated = USA.data.find((d) => d.date === '2021-04-07').people_vaccinated;
+    document.getElementById("admin").innerHTML = peopleVaccinated;
+});
